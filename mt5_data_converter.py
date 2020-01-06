53a0ffd3c093b00d11c2eaec4cc1c1a41b8bfa15
ALS777 <49898625+ALS777@users.noreply.github.com>
2020-01-06 20:26:19 +0000

    Update mt5_data_converter.py
    Small changes to enable up 80% faster conversion times and the addition of a progress check argument.

--- mt5_data_converter.py
+++ mt5_data_converter.py
@@ -1,9 +1,8 @@
 import pandas as pd
 import numpy as np
-from datetime import datetime
 
 
-def convert_tick(path, nrows=None):
+def convert_tick(path, nrows=None, progress_check=False):
     '''converts mt5 tick data into a pandas dataframe
 
     Converts tick data csv from mt5 into a pandas dataframe 
@@ -13,34 +12,38 @@ def convert_tick(path, nrows=None):
 
     Args:
         path:   path to csv file exported from mt5
-        rows (int):   number of rows of CSV to read in and convert
+        nrows (int):   number of rows of CSV to read in and convert
+        progress_check (bool): print progress statements
 
     Returns: 
         Pandas dataframe (index = datetime, columns = (bid, ask))
 
     '''
-
-    # reading in csv, converting it into a numpy array and flattening
+    
+    
+    #reading in csv, converting it into a numpy array and flattening
+    print('starting conversion...')
     df = pd.read_csv(path, nrows=nrows).to_numpy().flatten()
     date_time, bid, ask = [[] for i in range(3)]
-    loc = 0
-    for _ in range(len(df)):
+    
+    for loc, data in enumerate(df):
         # spliting relevent values into a list to index from
-        data = df[loc].split('\t')
+        data = data.split('\t')
         # if bid/ask missing fill with nan
         if data[2] == '': data[2] = np.nan
         if data[3] == '': data[3] = np.nan
-        date_time.append(datetime.strptime(f'{data[0]} {data[1]}', '%Y.%m.%d %H:%M:%S.%f'))
+        date_time.append(f'{data[0]} {data[1]}')
         bid.append(np.float(data[2]))
         ask.append(np.float(data[3]))
-        loc += 1
-    data = pd.DataFrame({'bid': bid, 'ask': ask}, index=date_time)
+        if progress_check and (loc/len(df)*100) % 2 == 0: print(f'Progress: {loc/len(df)}')
+    data = pd.DataFrame({'bid': bid, 'ask': ask}, index=pd.to_datetime(date_time))
     # forward filling nan values
     data.fillna(method='ffill', inplace=True)
+    print('conversion complete...')
     return data
     
 
-def convert_ohlc(path, nrows=None):
+def convert_ohlc(path, nrows=None, progress_check=False):
     '''converts mt5 ohlc data into a pandas dataframe
     
     Converts OHLC data from mt5 into a pandas dataframe
@@ -50,6 +53,7 @@ def convert_ohlc(path, nrows=None):
     Args:
         path:   path to csv file exported from mt5
         rows (int):   number of rows of CSV to read in and convert
+        progress_check (bool): print progress statements
 
     Returns:
         Pandas dataframe(index = datetime, columns = (open, high, low, close, volume, spread))
@@ -57,17 +61,18 @@ def convert_ohlc(path, nrows=None):
     '''
 
     # reading in csv, converting it into a numpy array and flattening
+    print('starting conversion...')
     df = pd.read_csv(path, nrows=nrows).to_numpy().flatten()
     date_time, open_, high, low, close, tickvol, spread = [[] for i in range(7)]
-    loc = 0
-    for _ in range(len(df)):
+    for loc, data in enumerate(df):
         # spliting relevent values into a list to index from
-        data = df[loc].split('\t')
-        date_time.append(datetime.strptime(f'{data[0]} {data[1]}', '%Y.%m.%d %H:%M:%S'))
+        data = data.split('\t')
+        date_time.append(f'{data[0]} {data[1]}')
         # appending data in to relevent lists
         [x.append(np.float(y)) for x,y in zip([open_, high, low, close, tickvol, spread], data[2:])]
-        loc += 1
+        if progress_check and (loc/len(df)*100) % 2 == 0: print(f'Progress: {loc/len(df)}')
     data = pd.DataFrame({'open': open_, 'high': high,
                          'low': low, 'close': close,
-                         'volume': tickvol, 'spread': spread}, index=date_time)
+                         'volume': tickvol, 'spread': spread}, index=pd.to_datetime(date_time))
+    print('conversion complete...')
     return data
